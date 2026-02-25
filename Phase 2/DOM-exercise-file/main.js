0f0be317d4d613eda31e86758732f73a54cad553
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-02-25 15:55:11 +0300

    a folder is deleted

--- Phase 2/DOM-exercise-file/main.js
+++
@@ -1,3 +0,0 @@
-//  your solutions start here
-//  make sure to connect your main.js file with your html 
-// happy coding 🧑‍💻
--- Phase 2/practice1/practice.js
+++ Phase 2/practice1/practice.js
@@ -1,94 +1,10 @@
-// variables.
-let form1 = document.getElementById("my-form1");
-let username = document.querySelector("#username");
-let pass = document.querySelector("#pass");
-let errorMsgHolder = document.getElementById("error-msg-holder") ;
-let theme = document.querySelector(".theme");
-let question1sample1 = document.querySelector("#sample1");
-let techCompanies = document.querySelector("#techCompanies");
-const yesBtn = document.getElementById("yes-btn");
-const noBtn = document.querySelector(".no");
-let adderForm = document.querySelector(".adder-form");
-let firstNumInput = document.querySelector(".first-num");
-let secondNumInput = document.querySelector(".second-num");
+const element = document.getElementById("change-btn")
 
-
-// functions;
-function submit (event) {
-  
-  if(username.value.trim() === "" && pass.value.trim()===""){
-    errorMsgHolder.style.display = "block";
-    errorMsgHolder.textContent = "missing both username & password";
-    errorMsgHolder.className= "display-error"
-    event.preventDefault();
-    }
-  else if(username.value.trim() === ""){
-    errorMsgHolder.textContent = "missing username";
-    errorMsgHolder.className = "display-error";
-    event.preventDefault();
-  }
-  else if(pass.value.trim()===""){
-    errorMsgHolder.textContent = "missing password";
-    errorMsgHolder.className = "display-error";
-    event.preventDefault();
-  }
-  else{
-    errorMsgHolder.textContent = "Successfully completed";
-    errorMsgHolder.className = "display-success";
-    event.preventDefault();
-  }
+let alerting =()=>{
+  console.log(element);
+  element.style.backgroundColor = "red";
+  element.style.color = "white";
+  element.style.border = "none";
+  console.log("hey why")
 }
-
-let changeTheme =()=>{
-  let body = document.body;
-  body.classList.toggle("black");
-}
-function blueBody (event) {
-  console.log("blueBody");
-  document.body.style.backgroundColor = "blue";
-}
-function noBody (){
-  document.body.style.backgroundColor = "";
-}
-function adder (event){
-  event.preventDefault();
-  console.log("two");
-  let firstNumInputValue = firstNumInput.value;
-  let secondNumInputValue = secondNumInput.value;
- console.log(firstNumInputValue + " and "+ secondNumInputValue);
-}
-form1.addEventListener("submit", submit);
-theme.addEventListener("click",changeTheme);
-yesBtn.addEventListener("click",blueBody);
-noBtn.addEventListener("click",noBody);
-adderForm.addEventListener("submit",adder);
-// // question 1
-// console.log(question1sample1);
-// console.log(question1sample1.textContent);
-// question 2
-// console.log(techCompanies);
-// let newLi = document.createElement("li");
-// newLi.textContent = "Amazon";
-// techCompanies.insertBefore(newLi, techCompanies.children[0]);
-// console.log(techCompanies);
-// newLi.classList.add("blue");
-// const blueCampaniesContainer = document.querySelector(".blueCampaniesContainer");
-// const blueCampanies = document.querySelectorAll("#techCompanies .blue");
-// console.log(blueCampanies);
-// lists = blueCampanies;
-// console.log(blueCampanies.length)
-// // console.log(blueCampanies[0]);
-// for(let i=0; i<blueCampanies.length;i++){
-//   blueCampaniesContainer.append(blueCampanies[i]);
-//   console.log("added");
-// }
-const iphoneBtn = document.querySelector(".iphone-btn");
-const appleContentContainer = document.querySelector(".apple-content-container");
-let appleImg = document.querySelector(".apple-img");
-
-function iphone(){
- console.log("iphone function");
-}
-iphoneBtn.addEventListener("click",iphone);
-console.log(iphone);
-console.log(iphoneBtn);
+element.addEventListener("click",alerting);
