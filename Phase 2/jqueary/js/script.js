30e833717ce8930f4d30f62cfd98163a6732c6c4
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-02-27 17:15:09 +0300

    todo app

---
+++ Phase 2/jqueary/js/script.js
@@ -0,0 +1,68 @@
+
+// variables
+let html = $("html");
+let body = $("body");
+let header = $("h1");
+let inputs = $("input");
+let listSec = $(".todo-list");
+let listContainer = $(".lists-container");
+let addSec = $(".todo-add")
+let description = $(".description");
+let addBtn = $(".add-btn");
+let inputAddBtn = $(".input-add-btn");
+let deleteBtn = $(".delete-btn");
+let todoForm = $("form");
+let taskInput = $("#task-input");
+// state
+html.css({
+  "font-size": "1.2rem",
+});
+description.css({
+  "font-weight": "bold",
+})
+addBtn.css({
+  display: "block",
+  margin: "auto"
+})
+inputs.css({
+  margin: "1rem",
+  padding: "1rem",
+  border: "1px solid grey"
+})
+addSec.hide();
+
+// Events
+addBtn.click(addTask);
+inputAddBtn.click(original)
+todoForm.submit(submitTask);
+deleteBtn.click(deleteTask);
+
+//functions
+function addTask(){
+  addSec.show();
+  addBtn.hide();
+}
+function original(){
+  addSec.hide();
+  addBtn.show();
+}
+function submitTask(event){
+  event.preventDefault();
+  let newTodo = document.createElement("li");
+  newTodo.className = "list";
+  let inputedTask = taskInput.val();
+  newTodo.innerHTML = 
+  `<div class="row">
+        <div class="description col-8 fw-bold">${inputedTask}
+        </div>
+        <button class="delete-btn btn col-3 p-2 bg-danger text-white fw-bold">
+          Delete
+        </button>
+      </div>`;
+  listContainer.append(newTodo);
+}
+
+function deleteTask(){
+  let todo = deleteBtn.parent().parent();
+  todo.hide();
+}
