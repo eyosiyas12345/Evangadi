cdc29f8b6b9b103330066e723486fa5cfa8f0a11
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-03-18 16:59:02 +0300

    phase 2 is to be completed

---
+++ Phase 2/post/scripts/data.js
@@ -0,0 +1,20 @@
+export const gameCards =[
+  {
+    id: 1,
+    title: "Snake Game",
+    thumbnail: "images/snake_thumbnail.jpg",
+    link : "../games/snake.html"
+  },
+  {
+    id: 2,
+    title: "Space Invaders",
+    thumbnail: "images/space_invander_thumbnail.jpg",
+    link : "../games/space_invaders.html"
+  },
+    {
+    id: 3,
+    title: "Rock Paper Scissors",
+    thumbnail: "images/rps_thumbnail.jpg",
+    link : "../games/rps.html"
+  }
+];
