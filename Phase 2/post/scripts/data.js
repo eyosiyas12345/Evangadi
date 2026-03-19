25da8c62f2c6b5946d50f792102df1705bc48d23
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-03-19 14:07:35 +0300

    post game files added

--- Phase 2/post/scripts/data.js
+++ Phase 2/post/scripts/data.js
@@ -3,18 +3,18 @@ export const gameCards =[
     id: 1,
     title: "Snake Game",
     thumbnail: "images/snake_thumbnail.jpg",
-    link : "../games/snake.html"
+    link : "../games/snake/snake.html"
   },
   {
     id: 2,
     title: "Space Invaders",
     thumbnail: "images/space_invander_thumbnail.jpg",
-    link : "../games/space_invaders.html"
+    link : "../games/space/space.html"
   },
     {
     id: 3,
     title: "Rock Paper Scissors",
     thumbnail: "images/rps_thumbnail.jpg",
-    link : "../games/rps.html"
+    link : "../games/rps/rps.html"
   }
 ];
